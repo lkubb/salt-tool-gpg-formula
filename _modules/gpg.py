e57dd7da20a5e1747762c5f450d215000155517c
jeanluc <lkubb@protonmail.com>
2026-05-19 22:08:50 +0200

    fix(state): Attempt to update expired subkeys

--- _modules/gpg.py
+++ _modules/gpg.py
@@ -416,8 +416,9 @@ def list_secret_keys(user=None, gnupghome=None, keyring=None):
 def _render_key(_key):
     tmp = {
         "keyid": _key["keyid"],
-        "uids": _key["uids"],
     }
+    if "uids" in _key:
+        tmp["uids"] = _key["uids"]
     if "fingerprint" in _key:
         tmp["fingerprint"] = _key["fingerprint"]
 
@@ -426,6 +427,7 @@ def _render_key(_key):
     length = _key.get("length", None)
     owner_trust = _key.get("ownertrust", None)
     trust = _key.get("trust", None)
+    subkeys = _key.get("subkey_info", {})
 
     if expires:
         tmp["expires"] = time.strftime(
@@ -440,6 +442,8 @@ def _render_key(_key):
         tmp["ownerTrust"] = LETTER_TRUST_DICT[_key["ownertrust"]]
     if trust:
         tmp["trust"] = LETTER_TRUST_DICT[_key["trust"]]
+    for data in subkeys.values():
+        tmp.setdefault("subkeys", []).append(_render_key(data))
     return tmp
 
 
